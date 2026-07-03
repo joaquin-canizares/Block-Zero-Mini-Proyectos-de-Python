10d8c62800cee81d85039a8882889c9f01a070a0
Joaquin Canizares <joaquincani2007@gmail.com>
2026-07-02 21:37:28 -0500

    Version local con funciones

---
+++ p1_parlay.py
@@ -0,0 +1,19 @@
+def pedir_cuota():
+    while True:
+        numero = input("Ingresa la cuota o listo para terminar: ")
+        if numero == 'listo':
+            break
+        else:
+            cuota = float(numero)  
+            if cuota <= 1 :
+                print("Ingrese una cuota mayor a 1")
+            else:
+                parlay = cuota * parlay
+
+def calcular_ganancia(monto, parlay):
+    monto = float(input("Ingrese monto a apostar: "))
+    supestaGanancia = monto * parlay
+    print("El total de ganancia de tu parlay es: ", supestaGanancia)
+    print("El total de cuota de tu parlay es: ", parlay)
+
+        
