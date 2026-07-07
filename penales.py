e95cab03960edd7d24019ffe5786f9f0e5b29415
Joaquin Canizares <joaquincani2007@gmail.com>
2026-07-07 00:14:09 -0500

    P3 completo y ajustes en P2

--- penales.py
+++ penales.py
@@ -2,6 +2,7 @@ import random
 def patear():
     goles = 0
     atajadas = 0
+    goles_maquina = 0
     for i in range (5):
         direccion = random.randint(1,3)
         lado = int(input("DISPARAR: 1=izquierda, 2=centro, 3=derecho: "))
@@ -14,11 +15,18 @@ def patear():
         atajar = int(input("ATAJAR: 1=izquierda, 2=centro, 3=derecho: "))
         if direccion_simulacion != atajar:
             print("GOL")
+            goles_maquina += 1
         else:
             print("Penal atajado!!!")
             atajadas += 1
     print("Has marcado estos goles: ", goles)  
-    print("Has atajado estos tiros: ", )         
+    print("Has atajado estos tiros: ", atajadas )   
+    if goles_maquina == goles:
+        print("Empate")
+    elif goles_maquina < goles:
+        print("Ganaste")
+    else:
+        print("Perdiste")      
 
-    
+
 patear()    
